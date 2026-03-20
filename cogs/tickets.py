0c75717fb4d3fc2b081e35f43c817c71f0b1b915
famobre <brenoviciado1@gmail.com>
2026-03-20 13:19:04 -0300

    corrigindo

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -331,7 +331,7 @@ class Tickets(commands.Cog):
             color=discord.Color.dark_gold()
         )
 
-        embed.set_image(url="c:\\Users\\breno\\Pictures\\fundo.png")
+        embed.set_image(url="https://imgur.com/a/PbKDDaj.png")
 
         await ctx.send(embed=embed, view=TicketView(self.bot))
 async def setup(bot: commands.Bot):
